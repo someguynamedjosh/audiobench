76fcf62e8b78473a366af6092dbfb58a0473125a
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2020-06-03 16:55:02 -0700

    I might have figured it out.

--- frontends/juce/build_win.ps1
+++ frontends/juce/build_win.ps1
@@ -13,7 +13,7 @@ mkdir _build -ea 0
 $Env:PROJECT_ROOT = Resolve-Path "..\.."
 $Env:RUST_OUTPUT_DIR = "$Env:PROJECT_ROOT\target\release"
 cd _build
-cmake ..
+cmake -G"MinGW Makefiles" -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_CONPILER=g++ ..
 cd ..
 cmake --build _build --config Release
 
