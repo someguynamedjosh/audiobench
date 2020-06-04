b23435d5c30a928286ee0b2b973c2e68ec74be29
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2020-06-03 17:28:18 -0700

    .

--- frontends/juce/build_win.ps1
+++ frontends/juce/build_win.ps1
@@ -13,6 +13,8 @@ mkdir _build -ea 0
 $Env:PROJECT_ROOT = Resolve-Path "..\.."
 $Env:RUST_OUTPUT_DIR = "$Env:PROJECT_ROOT\target\release"
 cd _build
+$Env:CC = "gcc"
+$Env:CXX = "g++"
 cmake -G"MinGW Makefiles" -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_CONPILER=g++ ..
 cd ..
 cmake --build _build --config Release
