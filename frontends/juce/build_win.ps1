6b8b0d30e9260e6983b804a2c1f573bb3b6f30f8
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2020-06-25 16:47:47 -0700

    After recovering from near death, I will now attempt to exact my revenge on the beast that is compiling for windows.

--- frontends/juce/build_win.ps1
+++ frontends/juce/build_win.ps1
@@ -1,24 +1,37 @@
 $ErrorActionPreference = "Stop"
 
 echo "This script will:"
-echo "1. Build Audiobench"
-echo "2. Build the JUCE frontend for Audiobench"
+echo "1. Build JUCE 6"
+echo "2. Build Audiobench"
+echo "3. Build the JUCE frontend for Audiobench"
 echo "This version of the script only makes a release version because I don't"
 echo "know how to use Powershell and I don't plan on learning."
+
+mkdir juce6_built -ea 0
+$Env:JUCE6_PREFIX = Resolve-Path "juce6_built"
+$Env:JUCE6_PREFIX = $Env:JUCE6_PREFIX -replace "\\", "/"
+cd juce_git
+cmake -Bcmake-build-install -DCMAKE_INSTALL_PREFIX="$Env:JUCE6_PREFIX" -G"MinGW Makefiles"
+cmake --build cmake-build-install --target install
+cd ..
+$Env:JUCE_DIR = "$Env:JUCE6_PREFIX/lib/cmake/JUCE-6.0.0"
+echo $JUCE_DIR
 
 cargo build --release
 mkdir artifacts -ea 0
 mkdir _build -ea 0
 
-$Env:PROJECT_ROOT = Resolve-Path "..\.."
-$Env:RUST_OUTPUT_DIR = "$Env:PROJECT_ROOT\target\release"
+$Env:PROJECT_ROOT = Resolve-Path "../.."
+# C compiler expects forward slashes.
+$Env:PROJECT_ROOT = $Env:PROJECT_ROOT -replace "\\", "/"
+$Env:RUST_OUTPUT_DIR = "$Env:PROJECT_ROOT/target/release"
 cd _build
-$Env:CC = "C:\ProgramData\chocolatey\bin\gcc.exe"
-$Env:CXX = "C:\ProgramData\chocolatey\bin\g++.exe"
-cmake -G"MinGW Makefiles" ..
+$Env:CC = "C:/ProgramData/chocolatey/bin/gcc.exe"
+$Env:CXX = "C:/ProgramData/chocolatey/bin/g++.exe"
+cmake -DJUCE_DIR="$Env:JUCE_DIR" -G"MinGW Makefiles" ..
 cd ..
 cmake --build _build --config Release
 
-Tree _build\Audiobench_artefacts\ /F
-cp _build\Audiobench_artefacts\Standalone\Audiobench.exe artifacts\Audiobench_Windows_x64_Standalone.exe
-cp _build\Audiobench_artefacts\VST3\Audiobench.vst3\Contents\x86_64-windows\Audiobench.dll artifacts\Audiobench_Windows_x64_VST3.dll
+Tree _build/Audiobench_artefacts/ /F
+cp _build/Audiobench_artefacts/Standalone/Audiobench.exe artifacts/Audiobench_Windows_x64_Standalone.exe
+cp _build/Audiobench_artefacts/VST3/Audiobench.vst3/Contents/x86_64-windows/Audiobench.dll artifacts/Audiobench_Windows_x64_VST3.dll
