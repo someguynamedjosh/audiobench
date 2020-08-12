49b11f393331d5e99276a10d950160443c1bfcb9
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2020-08-11 22:00:39 -0700

    Everything saves and loads correctly

--- src/registry/save_data.rs
+++ src/registry/save_data.rs
@@ -1,3 +1,4 @@
+use super::mini_bin::*;
 use super::Registry;
 use crate::engine::parts as ep;
 use crate::engine::static_controls as staticons;
@@ -5,229 +6,6 @@ use crate::util::*;
 use std::collections::{HashMap, HashSet};
 use std::io::{self, Write};
 use std::path::PathBuf;
-
-#[inline]
-fn compose_u2(bit1: bool, bit0: bool) -> u8 {
-    (if bit1 { 0b10 } else { 0b00 }) | (if bit0 { 0b01 } else { 0b00 })
-}
-
-#[inline]
-fn decompose_u2(u2: u8) -> (bool, bool) {
-    (u2 & 0b10 != 0, u2 & 0b01 != 0)
-}
-
-#[inline]
-fn compose_u4(bit3: bool, bit2: bool, bit1: bool, bit0: bool) -> u8 {
-    (if bit3 { 0b1000 } else { 0b0000 })
-        | (if bit2 { 0b0100 } else { 0b0000 })
-        | (if bit1 { 0b0010 } else { 0b0000 })
-        | (if bit0 { 0b0001 } else { 0b0000 })
-}
-
-#[inline]
-fn decompose_u4(u4: u8) -> (bool, bool, bool, bool) {
-    (
-        u4 & 0b1000 != 0,
-        u4 & 0b0100 != 0,
-        u4 & 0b0010 != 0,
-        u4 & 0b0001 != 0,
-    )
-}
-
-#[inline]
-fn ser_str(buffer: &mut Vec<u8>, text: &str) {
-    assert!(text.len() < std::u16::MAX as usize);
-    ser_u16(buffer, text.len() as u16);
-    buffer.reserve(text.len());
-    for b in text.bytes() {
-        buffer.push(b);
-    }
-}
-
-#[inline]
-fn ser_u2_slice(buffer: &mut Vec<u8>, value: &[u8]) {
-    if value.len() == 0 {
-        return;
-    }
-    let mut packed = vec![0; (value.len() + 3) / 4];
-    for (index, value) in value.iter().cloned().enumerate() {
-        debug_assert!(value <= 0b11);
-        packed[index / 4] |= value << (index % 4 * 2);
-    }
-    buffer.append(&mut packed);
-}
-
-#[inline]
-fn ser_u4_slice(buffer: &mut Vec<u8>, value: &[u8]) {
-    if value.len() == 0 {
-        return;
-    }
-    let mut packed = vec![0; (value.len() + 1) / 2];
-    for (index, value) in value.iter().cloned().enumerate() {
-        debug_assert!(value <= 0b1111);
-        packed[index / 2] |= value << (index % 2 * 4);
-    }
-    buffer.append(&mut packed);
-}
-
-#[inline]
-fn ser_u2_u6(buffer: &mut Vec<u8>, u2: u8, u6: u8) {
-    debug_assert!(u2 <= 0b11);
-    debug_assert!(u6 <= 0b111111);
-    buffer.push((u2 << 6) | u6);
-}
-
-#[inline]
-fn ser_u4_u12(buffer: &mut Vec<u8>, u4: u8, u12: u16) {
-    debug_assert!(u4 <= 0xF);
-    debug_assert!(u12 <= 0xFFF);
-    ser_u16(buffer, ((u4 as u16) << 12) | u12);
-}
-
-#[inline]
-fn ser_u8(buffer: &mut Vec<u8>, value: u8) {
-    buffer.push(value);
-}
-
-#[inline]
-fn ser_i16(buffer: &mut Vec<u8>, value: i16) {
-    for b in &value.to_be_bytes() {
-        buffer.push(*b);
-    }
-}
-
-#[inline]
-fn ser_u16(buffer: &mut Vec<u8>, value: u16) {
-    for b in &value.to_be_bytes() {
-        buffer.push(*b);
-    }
-}
-
-#[inline]
-fn ser_i32(buffer: &mut Vec<u8>, value: i32) {
-    for b in &value.to_be_bytes() {
-        buffer.push(*b);
-    }
-}
-
-#[inline]
-fn advance_des(slice: &mut &[u8], amount: usize) {
-    *slice = &slice[amount..];
-}
-
-#[inline]
-fn des_str(slice: &mut &[u8]) -> Result<String, ()> {
-    let len = des_u16(slice)? as usize;
-    if slice.len() < len {
-        return Err(());
-    }
-    let buffer = Vec::from(&slice[..len]);
-    advance_des(slice, len);
-    let string = String::from_utf8(buffer).map_err(|_| ())?;
-    Ok(string)
-}
-
-#[inline]
-fn des_u2_slice(slice: &mut &[u8], num_items: usize) -> Result<Vec<u8>, ()> {
-    if slice.len() < (num_items + 3) / 4 {
-        return Err(());
-    }
-    let mut res = Vec::with_capacity(num_items);
-    if num_items == 0 {
-        return Ok(res);
-    }
-    for index in 0..num_items {
-        res.push((slice[index / 4] >> (index % 4 * 2)) & 0b11);
-    }
-    advance_des(slice, (num_items + 3) / 4);
-    Ok(res)
-}
-
-#[inline]
-fn des_u4_slice(slice: &mut &[u8], num_items: usize) -> Result<Vec<u8>, ()> {
-    if slice.len() < (num_items + 1) / 2 {
-        return Err(());
-    }
-    let mut res = Vec::with_capacity(num_items);
-    if num_items == 0 {
-        return Ok(res);
-    }
-    for index in 0..num_items {
-        res.push((slice[index / 2] >> (index % 2 * 4)) & 0b1111);
-    }
-    advance_des(slice, (num_items + 1) / 2);
-    Ok(res)
-}
-
-#[inline]
-fn des_u2_u6(slice: &mut &[u8]) -> Result<(u8, u8), ()> {
-    let packed = des_u8(slice)?;
-    Ok((packed >> 6, packed & 0b111111))
-}
-
-#[inline]
-fn des_u4_u12(slice: &mut &[u8]) -> Result<(u16, u16), ()> {
-    let packed = des_u16(slice)?;
-    Ok((packed >> 12, packed & 0xFFF))
-}
-
-#[inline]
-fn des_u8(slice: &mut &[u8]) -> Result<u8, ()> {
-    if slice.len() < 1 {
-        return Err(());
-    }
-    let res = u8::from_be_bytes([slice[0]]);
-    advance_des(slice, 1);
-    Ok(res)
-}
-
-#[inline]
-fn des_i16(slice: &mut &[u8]) -> Result<i16, ()> {
-    if slice.len() < 2 {
-        return Err(());
-    }
-    let res = i16::from_be_bytes([slice[0], slice[1]]);
-    advance_des(slice, 2);
-    Ok(res)
-}
-
-#[inline]
-fn des_u16(slice: &mut &[u8]) -> Result<u16, ()> {
-    if slice.len() < 2 {
-        return Err(());
-    }
-    let res = u16::from_be_bytes([slice[0], slice[1]]);
-    advance_des(slice, 2);
-    Ok(res)
-}
-
-#[inline]
-fn des_i32(slice: &mut &[u8]) -> Result<i32, ()> {
-    if slice.len() < 4 {
-        return Err(());
-    }
-    let res = i32::from_be_bytes([slice[0], slice[1], slice[2], slice[3]]);
-    advance_des(slice, 4);
-    Ok(res)
-}
-
-#[inline]
-fn pack_value(value: f32, range: (f32, f32)) -> u16 {
-    let value = value.from_range(range.0, range.1);
-    // Value is now between 0 and 1.
-    let value = value.to_range(0.0, 0x10000 as f32).min(0xFFFF as f32);
-    // Value is now between 0 and 0xFFFF
-    value as u16
-}
-
-#[inline]
-fn unpack_value(value: u16, range: (f32, f32)) -> f32 {
-    let value = value as f32;
-    // Value is now between 0 and 0xFFFF
-    let value = value.from_range(0.0, 0xFFFF as f32);
-    // Value is now between 0 and 1.
-    value.to_range(range.0, range.1)
-}
 
 #[derive(Debug, Clone)]
 struct SavedAutomationLane {
@@ -424,26 +202,43 @@ impl SavedAutocon {
 
 #[derive(Debug, Clone)]
 struct SavedStaticon {
-    value: String,
+    value: Vec<u8>,
 }
 
 impl SavedStaticon {
     fn save(staticon: &staticons::Staticon) -> Self {
-        unimplemented!("This isn't ready.");
+        let mut value = Vec::new();
+        staticon.borrow_data().serialize(&mut value);
+        Self { value }
     }
 
     fn restore(&self, on: &mut staticons::Staticon) {
-        // TODO: Anything.
+        let mut data_slice = &self.value[..];
+        // Our own deserialize() method should have returned an error if the data's deserialization
+        // method would cause an error.
+        on.borrow_data_mut().deserialize(&mut data_slice).unwrap();
     }
 
     fn serialize(&self, buffer: &mut Vec<u8>) {
-        unimplemented!("This isn't ready.");
+        buffer.append(&mut self.value.clone());
     }
 
-    fn deserialize(slice: &mut &[u8]) -> Result<Self, ()> {
-        Ok(Self {
-            value: des_str(slice)?,
-        })
+    /// prototypical_dummy_control is a sacrificial value that should be a clone of the actual
+    /// control this SavedStaticon is storing data for. It will be used to determine how much data
+    /// should be saved from the slice to deserialize in the *actual* control during `restore`.
+    fn deserialize(
+        slice: &mut &[u8],
+        prototypical_dummy_control: staticons::Staticon,
+    ) -> Result<Self, ()> {
+        let old_slice = *slice;
+        // Basically have the dummy control deserialize the data and then just store the section
+        // of data it deserialized.
+        prototypical_dummy_control
+            .borrow_data_mut()
+            .deserialize(slice)?;
+        let data_len = old_slice.len() - slice.len();
+        let value = Vec::from(&old_slice[..data_len]);
+        Ok(Self { value })
     }
 }
 
@@ -653,6 +448,14 @@ impl SavedModule {
         let num_autocons = p.autocons.len();
         let num_staticons = p.staticons.len();
         Ok((num_inputs, num_autocons, num_staticons))
+    }
+
+    fn get_prototypical_dummy_staticons(
+        &self,
+        registry: &Registry,
+    ) -> Result<Vec<staticons::Staticon>, ()> {
+        let p = self.lookup_prototype(registry)?;
+        Ok(p.staticons.iter().map(|s| s.borrow().clone()).collect())
     }
 
     // The mode will indicate how data about this module is stored. Different modes take up
@@ -952,12 +755,14 @@ impl SavedModuleGraph {
             .map(|mode| SavedModule::deserialize(slice, *mode, &additional_libs))
             .collect::<Result<_, _>>()?;
 
-        let (mut num_inputs, mut num_autocons, mut num_staticons) = (0, 0, 0);
+        let (mut num_inputs, mut num_autocons) = (0, 0);
+        let mut prototypical_dummy_staticons = Vec::new();
         for module in &modules {
-            let (i, a, s) = module.get_requirements(registry)?;
+            let (i, a, _) = module.get_requirements(registry)?;
             num_inputs += i;
             num_autocons += a;
-            num_staticons += s;
+            let mut pdss = module.get_prototypical_dummy_staticons(registry)?;
+            prototypical_dummy_staticons.append(&mut pdss);
         }
 
         let input_modes = des_u2_slice(slice, num_inputs)?;
@@ -1007,8 +812,8 @@ impl SavedModuleGraph {
         }
 
         let mut staticons = Vec::new();
-        for _ in 0..num_staticons {
-            staticons.push(SavedStaticon::deserialize(slice)?);
+        for pdc in prototypical_dummy_staticons.into_iter() {
+            staticons.push(SavedStaticon::deserialize(slice, pdc)?);
         }
         let lane_modes = des_u4_slice(slice, num_lanes)?;
         let mut lanes = Vec::new();
