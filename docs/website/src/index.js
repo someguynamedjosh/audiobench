b2f3a75acf40da2df7f3bfb9eac4d78238b65617
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2020-09-21 18:55:30 -0700

    Website does stuff now.

---
+++ docs/website/src/index.js
@@ -0,0 +1,66 @@
+function transitionMenu(from, to) {
+    document.getElementById(from).classList.add('hidden');
+    setTimeout(() => {
+        document.getElementById(to).classList.remove('hidden');
+    }, 100);
+}
+
+function downloadURI(uri, name) {
+    var link = document.createElement("a");
+    // If you don't know the name or want to use
+    // the webserver default set name = ''
+    link.setAttribute('download', name);
+    link.href = uri;
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+}
+
+function linkClicked(name) {
+    if (name === 'getting-started') {
+        window.open('/book', '_blank');
+    } else if (name === 'github') {
+        window.open('https://github.com/joshua-maros/audiobench', '_blank');
+    } else if (name === 'sponsor') {
+        window.open('/sponsor.html', '_blank');
+    }
+}
+
+function startDownload(from, artifactUrl) {
+    let extension = artifactUrl.substr(artifactUrl.lastIndexOf('.') + 1);
+    let prettyArtifactName = 'Audiobench.' + extension;
+    let manualLink = document.getElementById('manual-link');
+    manualLink.href = artifactUrl;
+    manualLink.setAttribute('download', prettyArtifactName);
+    downloadURI(artifactUrl, prettyArtifactName);
+    document.getElementById(from).classList.add('hidden');
+    setTimeout(() => {
+        document.getElementById('download-menu-downloading').classList.remove('hidden');
+    }, 100);
+}
+
+function makeDownloadButton(name, parentMenu, artifactName) {
+    document.getElementById(name).addEventListener('click', event => {
+        startDownload(parentMenu, '/bin/' + artifactName);
+    });
+}
+
+window.addEventListener('DOMContentLoaded', event => {
+    document.getElementById('btn-windows').addEventListener('click', event => {
+        transitionMenu('download-menu-root', 'download-menu-windows');
+    });
+    document.getElementById('btn-macos').addEventListener('click', event => {
+        transitionMenu('download-menu-root', 'download-menu-macos');
+    });
+    document.getElementById('btn-linux').addEventListener('click', event => {
+        transitionMenu('download-menu-root', 'download-menu-linux');
+    });
+
+    makeDownloadButton('btn-windows-standalone', 'download-menu-windows', 'Audiobench_Windows_x64_Standalone.exe');
+    makeDownloadButton('btn-windows-vst3', 'download-menu-windows', 'Audiobench_Windows_x64_VST3.vst3');
+    makeDownloadButton('btn-macos-standalone', 'download-menu-macos', 'Audiobench_MacOS_x64_Standalone.dmg');
+    makeDownloadButton('btn-macos-au', 'download-menu-macos', 'Audiobench_MacOS_x64_AU.dmg');
+    makeDownloadButton('btn-macos-vst3', 'download-menu-macos', 'Audiobench_MacOS_x64_VST3.dmg');
+    makeDownloadButton('btn-linux-standalone', 'download-menu-linux', 'Audiobench_Linux_x64_Standalone.bin');
+    makeDownloadButton('btn-linux-vst3', 'download-menu-linux', 'Audiobench_Linux_x64_VST3.so');
+});
