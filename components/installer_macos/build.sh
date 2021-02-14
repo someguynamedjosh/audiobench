88a05c91e8e525f981fc3cb0ccc95d6ff2047f9d
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2021-02-13 17:23:07 -0800

    Installer woes

--- components/installer_macos/build.sh
+++ components/installer_macos/build.sh
@@ -15,11 +15,16 @@ cp $ARTIFACTS/libaudiobench_clib.dylib $TARGET/
 
 cd macos-installer-builder/macOS-x64/
 yes n | ./build-macos-x64.sh Audiobench $CRATE_VERSION
-ls 
-ls target/
-ls target/pkg
-ls target/*
 mkdir -p ../../artifacts/installer/
 mv target/pkg/*.pkg ../../artifacts/installer/Audiobench.pkg
+echo "1"
+ls ../../artifacts/installer/
+echo "2"
+ls target/pkg/
+cp target/pkg/*.pkg ../../artifacts/installer/Audiobench.pkg
+echo "3"
+ls ../../artifacts/installer/
+echo "4"
+ls target/pkg/
 
 rm -rf macos-installer-builder/
