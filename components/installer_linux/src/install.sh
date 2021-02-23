da7de2747fdc815be93183e3ff5911da5f597221
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2021-02-23 11:34:10 -0800

    Fix installer problems (fixes #21)

--- components/installer_linux/src/install.sh
+++ components/installer_linux/src/install.sh
@@ -5,7 +5,7 @@ echo ""
 echo "This installer will copy the following files:"
 echo "Audiobench.bin -> /usr/bin/"
 echo "Audiobench.vst3 -> /usr/lib/vst3/"
-echo "libaudiobench_clib.so -> /usr/lib/"
+echo "libaudiobench_clib.so.0 -> /usr/lib/"
 echo "AudiobenchStandalone.desktop -> /usr/share/applications/"
 echo "Julia 1.5 will also be installed, as it is inconsistently packaged across different distros."
 echo "If you are asked for your password, it is for permission to copy to these locations."
@@ -18,8 +18,8 @@ sudo mkdir -p /usr/lib/vst3
 sudo rm -rf /usr/lib/vst3/Audiobench.vst3
 sudo mv ./Audiobench.vst3 /usr/lib/vst3/Audiobench.vst3
 
-chmod +x ./libaudiobench_clib.so
-sudo mv ./libaudiobench_clib.so /usr/lib/
+chmod +x ./libaudiobench_clib.so.0
+sudo mv ./libaudiobench_clib.so.0 /usr/lib/
 
 sudo rm /usr/lib/libjulia.so*
 sudo cp -r ./julia/* /usr/
